d559008cb391cdf4192865726c7c4ca9fd45a75e
RakonaDev <juancajas1905@gmail.com>
2025-07-01 18:00:11 -0500

    d

--- src/database/Global.tsx
+++ src/database/Global.tsx
@@ -1,4 +1,12 @@
+
+/*
 export const Global = {
   api: 'http://localhost:8000/api',
   apiImagen: 'http://localhost:8000/'
+}
+*/
+
+export const Global = {
+  api: 'https://apicotizacion.exportando.online//api',
+  apiImagen: 'https://apicotizacion.exportando.online/'
 }
